4fcab8de423d81c034a1f06d90efaa469d11eea9
Aaron Fischer <aaron.fischer@caxiam.com>
2018-11-14 00:35:42 -0500

    Do not allow adding more ingredients until prior are filled in

--- src/ui/components/FieldIngredients/component.ts
+++ src/ui/components/FieldIngredients/component.ts
@@ -1,4 +1,26 @@
-import Component from '@glimmer/component';
+import Component, { tracked } from '@glimmer/component';
 
 export default class FieldIngredients extends Component {
+  @tracked errors: {} = {};
+
+  // just returns object if data doesnt exist
+  validate = data => {
+    const errors = {};
+    if (!data.amount) errors.amount = "Can't be blank";
+    if (!data.item) errors.item = "Can't be blank";
+    return errors;
+  };
+
+  onadd(field) {
+    const data = this.args.data;
+    if (data && data.length !== 0) {
+      const formErrors = this.validate(data[data.length - 1]);
+      this.errors = formErrors;
+      if (Object.keys(this.errors).length === 0) {
+        this.args.onadd(field);
+      }
+    } else {
+      this.args.onadd(field);
+    }
+  }
 }
